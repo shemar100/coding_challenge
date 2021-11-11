e35b6706ad4ca943ee4912eeb488c8e526f3d175
shemar100 <anderson.shemar17@gmail.com>
2021-11-11 01:04:05 -0500

    another way to write an anagram

--- anagram.py
+++ anagram.py
@@ -3,8 +3,46 @@
 # "public relations" is an anagram of "crap built on lies."
 # "clint eastwood" is an anagram of "old west action"
 def anagram(s1, s2):
-    s1 = s1.replace(" ", "").lower() # remove spaces and make lowercase
-    s2 = s2.replace(" ", "").lower() # remove spaces and make lowercase
+    # remove spaces and make lowercase
+    s1 = s1.replace(" ", "").lower() 
+    s2 = s2.replace(" ", "").lower()
+    
+    # Return boolean for sorted match.  
     return sorted(s1) == sorted(s2)
 
 print(anagram("clint eastwood", "old west action"))
+
+
+# doing the same thing with a dictionary
+def anagram_dict(s1, s2):
+    # remove spaces and make lowercase
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+    
+    # checl if the length of the strings are not equal
+    if len(s1) != len(s2):
+        return False
+    
+    # create a dictionary for each string
+    count = {}
+
+    for letter in s1: # loop through string 1
+        if letter in count:
+            count[letter] += 1 # add one to the value
+        else:
+            count[letter] = 1 # add the letter to the dictionary with a value of 1
+
+    for letter in s2: # loop through string 2
+        if letter in count:
+            count[letter] -= 1 # subtract one from the value
+        else:
+            count[letter] = 1 # add the letter to the dictionary with a value of 1  
+
+    for k in count: # loop through the dictionary
+        if count[k] != 0: # if the value is not 0
+            return False # return false
+
+    return True # return true if all values are 0
+
+    
+print(anagram_dict("clint eastwood", "old west action"))
